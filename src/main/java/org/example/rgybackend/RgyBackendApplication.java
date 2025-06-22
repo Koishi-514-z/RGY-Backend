582042be8bfcc8f0878633c272b1cc1ffd85b190
zsb <zsb_sjtu@sjtu.edu.cn>
2025-06-22 18:58:30 +0800

    initialize

---
+++ src/main/java/org/example/rgybackend/RgyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.rgybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rgybackend/RgyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rgybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
